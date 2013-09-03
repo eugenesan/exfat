36625a2bf41197f8f92a9e573cb0f6f2491c8872
Eugene San (eugenesan) <eugenesan@gmail.com>
2013-09-03 13:53:50 +0300

    Add suport for built-in kernel module

--- exfat_data.h
+++ exfat_data.h
@@ -53,8 +53,11 @@ extern "C" {
 #define FAT_CACHE_HASH_SIZE     64
 #define BUF_CACHE_SIZE          256
 #define BUF_CACHE_HASH_SIZE     64
-#define DEFAULT_CODEPAGE        437
-#define DEFAULT_IOCHARSET       "utf8"
+
+#ifndef CONFIG_EXFAT_DEFAULT_CODEPAGE
+#define CONFIG_EXFAT_DEFAULT_CODEPAGE	437
+#define CONFIG_EXFAT_DEFAULT_IOCHARSET	"utf8"
+#endif
 #ifdef __cplusplus
 }
 #endif
